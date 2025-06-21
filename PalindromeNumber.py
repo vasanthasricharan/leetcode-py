c6843e8980031dd11fbd344f1e2ad72a933cc1d7
Sri_Charan <154444424+vasanthasricharan@users.noreply.github.com>
2025-06-21 21:57:51 +0530

    PalindromeNumber.py

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,1 +1,9 @@
-
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        reversed_num = 0
+        while x > reversed_num:
+            reversed_num = reversed_num * 10 + x % 10
+            x //= 10
+        return x == reversed_num or x == reversed_num // 10
