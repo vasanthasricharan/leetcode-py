d1a449ead17c0f695683e8f1266fcba09d752fda
Sri_Charan <154444424+vasanthasricharan@users.noreply.github.com>
2025-06-21 22:20:49 +0530

    Add files via upload

---
+++ merge_sort.py
@@ -0,0 +1,25 @@
+def merge_sort(arr):
+    if len(arr)>1:
+        mid=len(arr)//2
+        left_half= arr[:mid]
+        right_half=arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+        i=j=k=0
+
+        while i< len(left_half) and j<len(right_half):
+            if left_half[i]<right_half[j]:
+                arr[k]=left_half[i]
+                i+=1
+            else:
+                arr[k]=right_half[j]
+                j+=1
+            k+=1
+        while i<len(left_half):
+            arr[k]=left_half[i]
+            i+=1
+            k+=1
+arr=list(map(int,input("Enter the element of the array by commas:").strip().split(',')))
+merge_sort(arr)
+print("Sorted array: ",arr)
